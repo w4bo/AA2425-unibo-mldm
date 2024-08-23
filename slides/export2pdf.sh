c64dbed7d8ae676abee1d12f4d8bc1595b57180b
Matteo Francia <m.francia@unibo.it>
2024-08-23 12:31:28 +0200

    try to export the slides directly to pdf

--- slides/export2pdf.sh
+++ slides/export2pdf.sh
@@ -2,5 +2,5 @@
 set -e
 for FILE in *.html; do 
     echo "Processing $FILE file...";
-    google-chrome --headless --disable-gpu --print-to-pdf="$FILE.pdf" "$FILE?print-pdf"
+    chrome --headless --disable-gpu --print-to-pdf="$FILE.pdf" "$FILE?print-pdf"
 done
