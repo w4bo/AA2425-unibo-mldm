7740391a62780e7e2e31c8287dfac940dec471a7
Matteo Francia <m.francia@unibo.it>
2024-08-23 10:23:01 +0200

    working on the ci

--- slides/generate.sh
+++ slides/generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 # for FILE in *.ipynb; do 
-for FILE in "lab-01-dataunderstanding.ipynb" "lab-04-datapreparation.ipynb"; do 
+for FILE in "lab-01-dataunderstanding.ipynb" "lab-02-datapreparation.ipynb"; do 
     echo "Processing $FILE file...";
     filename=$(basename -- "$FILE")
     extension="${filename##*.}"
     filename="${filename%.*}"
     jupyter nbconvert --execute --to notebook --inplace "$FILE"
     jupyter nbconvert "$FILE" --to slides
-    sed -i 's+</head>+<link rel="stylesheet" href="./mytheme.css" id="theme"></head>+g' "$filename.html"
-    mv "$filename.html" _site/
+    sed -i 's+</head>+<link rel="stylesheet" href="./mytheme.css" id="theme"></head>+g' "$filename.slides.html"
+    mv "$filename.slides.html" _site/
 done
