a4ebdaef171f5090c4fd82e1dcee7b9e414aa1f3
w4bo <you@example.com>
2024-08-27 16:47:57 +0000

    fix: ci

--- slides/export2pdf.sh
+++ slides/export2pdf.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 cd _site
+mkdir -p pdf
 for FILE in *.html; do 
     echo "Processing $PWD/$FILE file...";
     # chrome --headless=new --remote-allow-origins=* --disable-gpu --disable-software-rasterizer --print-to-pdf="$FILE.pdf" "$PWD/$FILE\?print-pdf"
-    docker run --rm -v "$(pwd)":/slides -v "$(pwd)":/home/user astefanutti/decktape /home/user/$FILE $FILE.pdf
+    docker run -v $(pwd):/home/user astefanutti/decktape /home/user/$FILE $FILE.pdf
+    # Copy the exported PDF from the latest used container to the local file system
+    docker cp `docker ps -lq`:slides/$FILE.pdf .
 done
