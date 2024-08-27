31f4f94532248776210b749faf257488c4fe2f40
Matteo Francia <m.francia@unibo.it>
2024-08-27 17:56:27 +0200

    fix: ci

--- slides/export2pdf.sh
+++ slides/export2pdf.sh
@@ -3,5 +3,5 @@ set -e
 cd _site
 for FILE in *.html; do 
     echo "Processing $PWD/$FILE file...";
-    chrome --headless=new --remote-allow-origins=* --disable-gpu --disable-software-rasterizer --print-to-pdf="$PWD/$FILE.pdf" "./$FILE?print-pdf"
+    chrome --headless=new --remote-allow-origins=* --disable-gpu --disable-software-rasterizer --print-to-pdf="$FILE.pdf" "$PWD/$FILE?print-pdf"
 done
