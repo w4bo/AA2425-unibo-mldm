6c28426ff58bdec19a7dcbd40072e6004e95aebe
Matteo Francia <m.francia@unibo.it>
2024-10-08 16:41:09 +0200

    fix the notebooks to generate

--- slides/generate.sh
+++ slides/generate.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 # for FILE in *.ipynb; do 
-for FILE in "lab-01-dataunderstanding.ipynb" "lab-02-housing.ipynb" "lab-03-datapreparation.ipynb" "lab-04-housing.ipynb" "lab-05-ml.ipynb" "lab-06-housing.ipynb"; do 
+# "lab-01-dataunderstanding.ipynb" "lab-02-housing.ipynb" "lab-03-datapreparation.ipynb" "lab-04-housing.ipynb" "lab-05-ml.ipynb" "lab-06-housing.ipynb"
+for FILE in *.ipynb; do 
     echo "Processing $FILE file...";
+    if [[ "$FILE" == *"lab-00"* ]]; then
+        echo "Skipping $FILE"
+        continue
+    fi
     filename=$(basename -- "$FILE")
     extension="${filename##*.}"
     filename="${filename%.*}"
